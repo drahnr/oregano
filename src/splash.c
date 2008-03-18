20c6bb660aeb3f1d9c0d07beb67b27529308214a
maxy <adrian@Max.local>
2008-03-18 19:16:00 +0000

    missing copyright in splash
    darcs-hash:20080318191636-e47e8-2de58a343e0367275711858127461013666623f5.gz

--- src/splash.c
+++ src/splash.c
@@ -1,3 +1,28 @@
+/*
+ * splash.c
+ *
+ *
+ * Author:
+ *  Ricardo Markiewicz <rmarkie@fi.uba.ar>
+ *
+ * Copyright (C) 2003-2008  Ricardo Markiewicz
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 
 #include <unistd.h>
 #include <glade/glade.h>
--- src/splash.h
+++ src/splash.h
@@ -1,3 +1,27 @@
+/*
+ * splash.h
+ *
+ *
+ * Author:
+ *  Ricardo Markiewicz <rmarkie@fi.uba.ar>
+ *
+ * Copyright (C) 2003-2008  Ricardo Markiewicz
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #ifndef _OREGANO_SPLASH_H_
 #define _OREGANO_SPLASH_H_
