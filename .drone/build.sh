d5a7dc727ced8e929c83cf9530de9da9f3d8d09c
Bernhard Schuster <schuster.bernhard@gmail.com>
2014-12-25 14:18:10 +0100

    ci: Disable automatic rpm generation for now.

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -e
 ./waf configure --prefix="/usr" debug install
-rpmdev-setuptree
-cp oregano.spec.in ~/rpmbuild/SPECS/oregano.spec
-cd ~/rpmbuild/
-rpmbuild -ba SPECS/oregano.spec
-rpmlint RPMS/x86_64/oregano*.rpm
+
+
+#rpmdev-setuptree
+#cp oregano.spec.in ~/rpmbuild/SPECS/oregano.spec
+
+#cd ~/rpmbuild/SOURCES
+#wget https://github.com/drahnr/oregano/archive/master.tar.gz
+
+#cd ~/rpmbuild/
+#rpmbuild -ba SPECS/oregano.spec
+#rpmlint RPMS/x86_64/oregano*.rpm
 
