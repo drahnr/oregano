a4af69f9e6124bb832d5c6508f0d9cacd0cbf196
Bernhard Schuster <schuster.bernhard@gmail.com>
2014-12-25 13:57:51 +0100

    ci: bogus commit for drone.

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+set -e
 scp ~/rpmbuild/RPMS/x86_64/oregano*.rpm http://drone.beerbach.me:/srv/rpms/oregano
 scp ~/rpmbuild/RPMS/x86_64/oregano*.rpm http://drone.beerbach.me:/srv/repo/
