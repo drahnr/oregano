add45b3d234af4372c9425c3e8da9ce663b6a270
Bernhard Schuster <schuster.bernhard@gmail.com>
2014-12-25 14:38:34 +0100

    ci: Disable deployment for now.

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-scp ~/rpmbuild/RPMS/x86_64/oregano*.rpm http://drone.beerbach.me:/srv/rpms/oregano
-scp ~/rpmbuild/RPMS/x86_64/oregano*.rpm http://drone.beerbach.me:/srv/repo/
+#scp ~/rpmbuild/RPMS/x86_64/oregano*.rpm http://drone.beerbach.me:/srv/rpms/oregano
+#scp ~/rpmbuild/RPMS/x86_64/oregano*.rpm http://drone.beerbach.me:/srv/repo/
