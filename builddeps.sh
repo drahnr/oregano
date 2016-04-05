ec1aa8e2fa91687280c50658744d5321a26efc8a
Bernhard Schuster <bernhard@ahoi.io>
2016-04-05 07:36:39 +0200

    ci: Only ci shall do a system update, closes #154.

--- builddeps.sh
+++ builddeps.sh
@@ -4,12 +4,32 @@ distribution=$(cat /etc/os-release |grep "^NAME=" |cut -d '=' -f 2)
 
 echo "Distribution detected is \"${distribution}\""
 
+MODE="$1"
+
+if [ ${MODE:="interactive"} != "ci" ]; then
+	MODE="interactive"
+fi
+
+echo "Running $0 in $MODE mode"
+
+
 if [ "${distribution}" = "\"Ubuntu\"" ]; then # idiots...
-	apt-get update -y
-	apt-get install -y python libglib2.0-dev intltool libgtk-3-dev libxml2-dev libgoocanvas-2.0-dev libgtksourceview-3.0-dev gcc clang
+	PKGS="python libglib2.0-dev intltool libgtk-3-dev libxml2-dev libgoocanvas-2.0-dev libgtksourceview-3.0-dev gcc clang"
+	if [ ${MODE} = "ci" ]; then
+		apt-get update -y
+		apt-get install -y $PKGS
+	else
+		apt-get install $PKGS
+	fi
 elif [ "${distribution}" = "Fedora" ]; then
-	dnf update -y
-	dnf install -y python gtk3-devel libxml2-devel gtksourceview3-devel intltool glib2-devel goocanvas2-devel desktop-file-utils clang gcc
+	PKGS="python gtk3-devel libxml2-devel gtksourceview3-devel intltool glib2-devel goocanvas2-devel desktop-file-utils clang gcc"
+
+	if [ ${MODE} = "ci" ]; then
+		dnf update -y
+		dnf install -y $PKGS
+	else
+		dnf install $PKGS
+	fi
 else
 	echo "No idea what to do.. trying to run regular builddeps"
 fi
