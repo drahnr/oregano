67a3fa32a57fb889fb5b41dc045f2572c773d1d7
Bernhard Schuster <bernhard@ahoi.io>
2017-05-17 13:32:38 +0200

    ci: Fix rpm generation

--- .concourse/fedora/buildrpm.sh
+++ .concourse/fedora/buildrpm.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-dnf install -y pyliblzma pxz
-pwd
-mkdir -p rpmbuild/{SOURCES,BUILD,RPMS,SPECS} || echo "Failed to create directories: $?"
-cp oregano.spec rpmbuild/SPECS/ || echo "Failed to copy spec file $?"
-# ./waf dist is too stupid to know xz ...
-git archive --format=tar --prefix=oregano/ HEAD | xz > rpmbuild/SOURCES/oregano-0.83.3.tar.xz || echo "Failed to create dist file $?"
-cd rpmbuild || echo "Failed to cd to rpmbuild $?"
+mkdir -p rpmbuild/{SOURCES,BUILD,RPMS,SPECS}
+cp oregano.spec rpmbuild/SPECS/
+./waf dist
+mv oregano*.tar.xz rpmbuild/SOURCES/
+cd rpmbuild
 rpmbuild \
---define "_topdir %(pwd)/rpmbuild" \
+--define "_topdir %(pwd)" \
 --define "_builddir %{_topdir}/BUILD" \
 --define "_rpmdir %{_topdir}/RPMS" \
 --define "_srcrpmdir %{_topdir}/RPMS" \
