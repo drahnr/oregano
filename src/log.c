a6e11a999331ba81e0d056553492cb018a9c66c1
Michael Banucu <michael.banucu@googlemail.com>
2017-05-25 18:49:15 +0200

    Log: connect logstore with logview, closes #174

--- src/log.c
+++ src/log.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "log.h"
 
 #define LOG_GET_PRIVATE(object) (G_TYPE_INSTANCE_GET_PRIVATE ((object), LOG_TYPE, LogPrivate))
@@ -30,17 +31,54 @@ static void log_init (Log *self)
 Log *log_new () { return g_object_new (LOG_TYPE, NULL); }
 
 /**
- * show on top of the items in the logview but
- * if exported at the end of the exported debug output
+ * trim only newlines
+ */
+gchar *log_append_trim_message(const gchar *string) {
+	if (string == NULL)
+		return NULL;
+	gchar **array = g_regex_split_simple("[\\r\\n]*(.*?)[\\n$]+", string, 0, 0);
+	gchar *ret_val = g_strdup(array[0]);
+	g_strfreev(array);
+	if (*ret_val == 0) {
+		g_free(ret_val);
+		ret_val = NULL;
+	}
+	return ret_val;
+}
+
+/**
+ * Show on top of the items in the logview.
+ *
+ * If the previous message had an '\r' at the end, the previous message will be
+ * replaced by the new message instead of prepending the new message.
+ *
+ * Leading and trailing '\n's will be truncated.
  */
 void log_append (Log *log, const gchar *prefix, const gchar *message)
 {
-	g_return_if_fail (message != NULL);
+	//trim message (only newlines), because newlines do not make sense in the log view
+	gchar *message_trimed = log_append_trim_message(message);
+	if (message_trimed == NULL)
+		return;
 
-	GtkTreeIter item;
-	gtk_list_store_insert (GTK_LIST_STORE (log), &item, 0);
-	gtk_list_store_set (GTK_LIST_STORE (log), &item, 0, g_strdup (prefix), 1, g_strdup (message),
-	                    -1);
+	GtkTreeIter iter;
+	//if the previous message had an '\r' at the end, the previous message will be
+	//replaced by the new message instead of prepending the new message
+	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(log), &iter)) {
+		GValue previous_value;
+		previous_value.g_type = 0;
+		gtk_tree_model_get_value(GTK_TREE_MODEL(log), &iter, 1, &previous_value);
+		const gchar *previous_message = g_value_get_string(&previous_value);
+
+		if (previous_message != NULL && previous_message[strlen(previous_message) - 1] != '\r')
+			gtk_list_store_prepend (GTK_LIST_STORE (log), &iter);
+		g_value_unset(&previous_value);
+	} else {
+		gtk_list_store_prepend (GTK_LIST_STORE (log), &iter);
+	}
+	gtk_list_store_set (GTK_LIST_STORE (log), &iter, 0, g_strdup (prefix), 1, g_strdup (message_trimed),
+                        -1);
+	g_free(message_trimed);
 }
 
 void log_append_error (Log *log, const gchar *prefix, const gchar *message, GError *error)
--- wscript
+++ wscript
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # encoding: utf-8
 
-VERSION = '0.84.2'
+VERSION = '0.84.3'
 APPNAME = 'oregano'
 
 top = '.'
