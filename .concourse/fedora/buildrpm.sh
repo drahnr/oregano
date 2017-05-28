3995608bac3c1b26a70cfcf9e38a8f0e780d943c
Bernhard Schuster <bernhard@ahoi.io>
2017-05-28 08:38:34 +0200

    ci: Adjust rpm creation

--- .concourse/fedora/buildrpm.sh
+++ .concourse/fedora/buildrpm.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -p rpmbuild/{SOURCES,BUILD,RPMS,SPECS}
+mkdir -p rpmbuild/{SOURCES,BUILD,RPMS,SRPMS,SPECS}
 ./waf configure rpmspec
 cp -v build/rpmspec/oregano.spec rpmbuild/SPECS/
 ./waf dist
@@ -12,7 +12,9 @@ rpmbuild \
 --define "_topdir %(pwd)" \
 --define "_builddir %{_topdir}/BUILD" \
 --define "_rpmdir %{_topdir}/RPMS" \
---define "_srcrpmdir %{_topdir}/RPMS" \
+--define "_srcrpmdir %{_topdir}/SRPMS" \
 --define "_specdir %{_topdir}/SPECS" \
 --define "_sourcedir  %{_topdir}/SOURCES" \
 -ba SPECS/oregano.spec && echo "RPM was built" && pwd
+
+exit 2
