a1ce8eadb8d1506864a4c00fdc8559a0ea4b853f
Michael Banucu <michael.banucu@googlemail.com>
2017-06-17 22:31:34 +0200

    test CI, analyzes #182

--- test/test_engine_ngspice.c
+++ test/test_engine_ngspice.c
@@ -111,7 +111,7 @@ static gchar* test_engine_ngspice_get_test_dir_base() {
 }
 
 static void test_engine_ngspice_basic() {
-
+asdf
 	TestEngineNgspiceResources *test_resources = test_engine_ngspice_resources_new();
 
 	gchar *test_dir = test_engine_ngspice_get_test_dir_base();
